acaa1d0995517d060acdc8d4b63406209c989ae7
Christian Ndu <christiannduh@gmail.com>
2021-08-20 00:07:39 +0100

    reached mvp

--- main.go
+++ main.go
@@ -1,27 +1,58 @@
 package main
 
 import (
-	"fmt"
 	"hngcv/name"
+
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
 
-	"net/http"
+	"github.com/gorilla/mux"
 )
 
+type Data struct {
+	Name string `json:"name"`
+	Email string `json:"email"`
+	Message string `json:"message"`
+}
 
 func main() {
 	
+	r := mux.NewRouter()
+
 	name.PrintName()
 
-	http.Handle("/", http.FileServer(http.Dir("./client")))
+	r.Handle("/", http.FileServer(http.Dir("./client")))
+	r.HandleFunc("/form", HandleForm).Methods("POST")
+
 	fmt.Println("listening...")
+
 	port := os.Getenv("PORT")
 	if port == ""{
 		port = "8080"
 	}
-   err := http.ListenAndServe(":"+port, nil)
+   err := http.ListenAndServe(":"+port, r)
    if err != nil{
 	   log.Fatal("server error: ", err)
    }
+}
+
+func HandleForm( w http.ResponseWriter, r *http.Request ){
+	w.Header().Set("Content-Type", "application/json")
+	var data Data 
+	_ = json.NewDecoder(r.Body).Decode(&data) 
+
+	go WriteToJSON(data)
+	json.NewEncoder(w).Encode(data) 
+}
+
+func  WriteToJSON (data Data) {
+	file, err := json.MarshalIndent(data, "", " ") 
+	if err != nil {
+		log.Fatal("ERROR while writing to file", err)
+	}
+	_ = ioutil.WriteFile("formresponse.json", file, 0644)
 }
